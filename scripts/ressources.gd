ed1d935b7a68157acb0b9a0a60cf95c0156a0d47
sami-hmr <sami12022005@yahoo.fr>
2026-02-07 17:19:42 +0100

    feat: timer resource

--- scripts/ressources.gd
+++ scripts/ressources.gd
@@ -2,18 +2,23 @@ class_name Resources
 extends Node2D
 
 @export
-var resource_list: Dictionary[String, int] = {
+var resource_list: Dictionary = {
 	"wood": 0,
 	"stone": 0,
 	"iron": 0,
 	"juice": 0,
+	"time": 0
 }
 
 signal resource_updated(name: String, val: int)
 
+
 func update_resource(name: String, val: int):
 	resource_list[name] += val
 	emit_signal("resource_updated", name, val)
+
+func set_resource(name: String, val: int):
+	resource_list[name] = val
 
 func get_resource(name: String) -> int:
 	return resource_list[name]
