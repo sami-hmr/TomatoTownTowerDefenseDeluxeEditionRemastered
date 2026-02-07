bc843a3351761011d2ad95e7af2a6ca17f3bbb15
sami-hmr <sami12022005@yahoo.fr>
2026-02-07 13:15:23 +0100

    feat: zsqd camera movements

--- scripts/player_camera.gd
+++ scripts/player_camera.gd
@@ -4,13 +4,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 
+
 func _physics_process(delta: float) -> void:
-	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_x := Input.get_axis("left", "right")
 	if direction_x:
 		velocity.x = direction_x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	var direction_y := Input.get_axis("ui_up", "ui_down")
+	var direction_y := Input.get_axis("up", "down")
 	if direction_y:
 		velocity.y = direction_y * SPEED
 	else:
