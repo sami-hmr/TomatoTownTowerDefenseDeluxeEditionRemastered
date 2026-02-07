509d50e8b944b3752ca1a102c31da0f8d8b0dafc
JulesDELTEIL <jules.delteil@epitech.eu>
2026-02-07 15:09:33 +0100

    feat(resources): update resources to make them reusable

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,32 +1,20 @@
 extends Node2D
 @onready var ressource_container: HBoxContainer = $"../CanvasLayer/ressource_container"
 
-var resources : Dictionary[String, int] = {
-	"wood": 0,
-	"stone": 0,
-	"iron": 0,
-	"juice" : 0
-}
+@onready var res: Resources = $"../Resources"
 
-signal addResources(new_resources: Dictionary[String, int])
-signal setResources(new_resources: Dictionary[String, int])
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	setup_resources()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	update_resources()
 
 func setup_resources():
 	var resources_data = [
-		{"frames": preload("res://Ressources/wood.tres"), "name": "wood", "amount": resources["wood"]},
-		{"frames": preload("res://Ressources/stone.tres"), "name": "stone", "amount": resources["stone"]},
-		{"frames": preload("res://Ressources/iron.tres"), "name": "iron", "amount": resources["iron"]},
-		{"frames": preload("res://Ressources/juice.tres"), "name": "juice", "amount": resources["juice"]}
+		{"frames": preload("res://Ressources/wood.tres"), "name": "wood", "amount": res.get_resource("wood")},
+		{"frames": preload("res://Ressources/stone.tres"), "name": "stone", "amount": res.get_resource("stone")},
+		{"frames": preload("res://Ressources/iron.tres"), "name": "iron", "amount": res.get_resource("iron")},
+		{"frames": preload("res://Ressources/juice.tres"), "name": "juice", "amount": res.get_resource("juice")}
 		]
 
 	for data in resources_data:
@@ -37,13 +25,4 @@ func setup_resources():
 func update_resources():
 	var children : Array[Node] = ressource_container.get_children()
 	for child in children:
-		child.value = resources.get(child.resource_name, child.value)
-
-func _on_add_resources(new_resources: Dictionary[String, int]) -> void:
-	for resource_name in new_resources:
-		resources[resource_name] += new_resources[resource_name]
-
-
-func _on_set_resources(new_resources: Dictionary[String, int]) -> void:
-	for resource_name in new_resources:
-		resources[resource_name] = new_resources[resource_name]
+		child.value = res.get_resource(child.resource_name)
---
+++ scripts/ressources.gd
@@ -0,0 +1,25 @@
+class_name Resources
+extends Node2D
+
+@export
+var resource_list: Dictionary[String, int] = {
+	"wood": 0,
+	"stone": 0,
+	"iron": 0,
+	"juice": 0,
+}
+
+signal resource_updated(name: String, val: int)
+
+func add_resource(name: String, val: int):
+	resource_list[name] += val
+	emit_signal("resource_updated", name, val)
+
+func get_resource(name: String) -> int:
+	return resource_list[name]
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
